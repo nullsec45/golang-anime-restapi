b840eb270a7ae46214f5cff3ee152904a542de0f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-03 16:57:59 +0700

    feat : dto response.

---
+++ dto/response.go
@@ -0,0 +1,45 @@
+package dto
+
+type ResponseSuccess[T any] struct {
+	Code   int      `json:"code"`	
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+type ResponseError[T any] struct {
+	Code   int      `json:"code"`	
+	Message string      `json:"message"`
+	Error    interface{} `json:"error,omitempty"`
+}
+
+func CreateResponseError(message string) ResponseError [string]{
+	return ResponseError[string]{
+		Code : 400,
+		Message: message,	
+		// Error: "",
+	}
+}
+
+func CreateResponseErrorData(message string, error map[string]string) ResponseError [string]{
+	return ResponseError[string]{
+		Code : 422,
+		Message: message,	
+		Error: error,
+	}
+}
+
+func CreateResponseSuccess(message string) ResponseSuccess [string]{
+	return ResponseSuccess[string]{
+		Code : 200,
+		Message: message,	
+		// Data: data,
+	}
+}
+
+func CreateResponseSuccessWithData[T any](message string, data T) ResponseSuccess [T]{
+	return ResponseSuccess[T]{
+		Code : 200,
+		Message: message,	
+		Data: data,
+	}
+}
