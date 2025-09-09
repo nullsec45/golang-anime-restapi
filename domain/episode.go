f1a94773d760f90d52ccaebe4a1a84e993df7e90
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-10 00:32:31 +0700

    feat :episode domain

---
+++ domain/episode.go
@@ -0,0 +1,37 @@
+package domain
+
+import (
+	"context"
+	"database/sql"
+	"time"
+	"github.com/nullsec45/golang-anime-restapi/dto"
+)
+
+type AnimeEpisode struct {
+	Id              string        `db:"id"`
+	AnimeId         string        `db:"anime_id"`
+	Number          int           `db:"number"`
+	SeasonNumber    int           `db:"season_number"`     
+	Title           string        `db:"title"`             
+	Synopsis        string        `db:"synopsis"`          
+	AirDate         sql.NullTime  `db:"air_date"` 
+	DurationMinutes sql.NullTime  `db:"duration_minutes"`  
+	IsSpecial       bool          `db:"is_special"`
+	CreatedAt       time.Time     `db:"created_at"`
+	UpdatedAt       time.Time     `db:"updated_at"`
+}
+
+type AnimeEpisodeRepository interface {
+	FindByAnimeId(ctx context.Context, id string) ([]AnimeEpisode, error)
+	FindById(ctx context.Context, animeId string, id string) (AnimeEpisode, error)
+	Save(ctx context.Context, data []AnimeEpisode) error
+	DeleteByAnimeId(ctx context.Context, animeId string) error
+	DeleteById(ctx context.Context, id []string) error
+}
+
+type AnimeEpisodeService interface {
+	Create(ctx context.Context, req dto.CreateAnimeEpisodeRequest) error
+	DeleteByAnimeId(ctx context.Context, animeId string) error
+	DeleteById(ctx context.Context, id string) error
+	// Delete(ctx context.Context, req dto.DeleteAnimeEpisodeRequest) error
+}
