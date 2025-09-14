fe83e7f4076968c5c253f3e5fd6a0d91f40f06b1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-14 10:30:52 +0700

    migration  : add column deleted_at to genres table.

--- database/migrations/20250912160020_add_column_time_genres_table.down.sql
+++ database/migrations/20250912160020_add_column_time_genres_table.down.sql
@@ -1,2 +1,8 @@
-ALTER TABLE genres DROP COLUMN created_at;
-ALTER TABLE genres DROP COLUMN updated_at; 
+BEGIN;
+
+ALTER TABLE IF EXISTS genres
+  DROP COLUMN IF EXISTS created_at,
+  DROP COLUMN IF EXISTS updated_at,
+  DROP COLUMN IF EXISTS deleted_at;
+
+COMMIT;
