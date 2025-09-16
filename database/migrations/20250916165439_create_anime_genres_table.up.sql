cb38c1cb47efd127c95b1c1b8202fc25ed88c2d5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-17 01:26:19 +0700

    migration : create anime genres table

---
+++ database/migrations/20250916165439_create_anime_genres_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE anime_genres (
+  anime_id UUID NOT NULL REFERENCES animes(id) ON DELETE CASCADE,
+  genre_id UUID NOT NULL REFERENCES genres(id) ON DELETE CASCADE,
+  created_at    TIMESTAMPTZ NULL DEFAULT now(),
+  updated_at    TIMESTAMPTZ NULL DEFAULT now(),
+  PRIMARY KEY (anime_id, genre_id)
+);
