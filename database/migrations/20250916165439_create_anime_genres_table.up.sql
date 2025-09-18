03a91d68a3eccefa230ee7845adf95b72b411b28
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-18 20:09:56 +0700

    migration : add column id to anime genres table.

--- database/migrations/20250916165439_create_anime_genres_table.up.sql
+++ database/migrations/20250916165439_create_anime_genres_table.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE anime_genres (
+  id UUID NOT NULL,
   anime_id UUID NOT NULL REFERENCES animes(id) ON DELETE CASCADE,
   genre_id UUID NOT NULL REFERENCES genres(id) ON DELETE CASCADE,
   created_at    TIMESTAMPTZ NULL DEFAULT now(),
   updated_at    TIMESTAMPTZ NULL DEFAULT now(),
-  PRIMARY KEY (anime_id, genre_id)
+  PRIMARY KEY (id,anime_id, genre_id)
 );
