35a1ed068d5d587e88ad6a7621b58b9bb315ae38
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-18 20:18:51 +0700

    chore : add constant error anime genres not found  and anim genres already

--- domain/error.go
+++ domain/error.go
@@ -4,7 +4,9 @@ import "errors"
 
 var AnimeNotFound=errors.New("Anime not found!.")
 var AnimeEpisodeNotFound=errors.New("Anime episode not found!.")
-var AnimeGenreNotFound=errors.New("Anime genre episode not found!.")
+var AnimeGenreNotFound=errors.New("Anime genre not found!.")
+var AnimeGenresNotFound=errors.New("Anime genres not found!.")
 var AuthFail=errors.New("Authentication Failed!.")
 var EmailRegister=errors.New("Email already register!.")
 var PasswordNotMatch=errors.New("Password and Confirm Password not matching!.")
+var AnimeGenresAlready=errors.New("Anime Genres already in database!")
