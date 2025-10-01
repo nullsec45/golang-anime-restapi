afc1434297544c0d5873ca205478808b4f28af2f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-01 17:14:24 +0700

    migration : create table anime studio

---
+++ database/migrations/20251001052508_create_table_anime_studio.up.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS anime_studios (
+  anime_id  UUID NOT NULL REFERENCES animes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  studio_id UUID NOT NULL REFERENCES studios(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  role      TEXT NOT NULL CHECK (role IN ('Animation','Producer','Licensor','Publisher')),
+  PRIMARY KEY (anime_id, studio_id, role)
+);
+
+CREATE OR REPLACE FUNCTION hard_delete_links_on_soft_delete()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  -- Jalan hanya ketika deleted_at berubah dari NULL -> NOT NULL
+  IF NEW.deleted_at IS NOT NULL AND (OLD.deleted_at IS DISTINCT FROM NEW.deleted_at) THEN
+    IF TG_TABLE_NAME = 'studios' THEN
+      DELETE FROM anime_studios WHERE studio_id = NEW.id;
+    ELSIF TG_TABLE_NAME = 'animes' THEN
+      DELETE FROM anime_studios WHERE anime_id = NEW.id;
+    END IF;
+  END IF;
+  RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS trg_studios_soft_delete ON studios;
+CREATE TRIGGER trg_studios_soft_delete
+AFTER UPDATE OF deleted_at ON studios
+FOR EACH ROW
+WHEN (NEW.deleted_at IS NOT NULL AND (OLD.deleted_at IS DISTINCT FROM NEW.deleted_at))
+EXECUTE FUNCTION hard_delete_links_on_soft_delete();
+
+DROP TRIGGER IF EXISTS trg_animes_soft_delete ON animes;
+CREATE TRIGGER trg_animes_soft_delete
+AFTER UPDATE OF deleted_at ON animes
+FOR EACH ROW
+WHEN (NEW.deleted_at IS NOT NULL AND (OLD.deleted_at IS DISTINCT FROM NEW.deleted_at))
+EXECUTE FUNCTION hard_delete_links_on_soft_delete();
