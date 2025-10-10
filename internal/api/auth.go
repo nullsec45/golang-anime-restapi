cebce379f320efe2fbdc40a649e2d3e395187ba6
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-10 17:24:33 +0700

    feat(auth) : add log in auth login.

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -9,6 +9,8 @@ import (
 	"github.com/nullsec45/golang-anime-restapi/domain"
 	"github.com/nullsec45/golang-anime-restapi/dto"
 	"github.com/nullsec45/golang-anime-restapi/internal/utility"
+	"github.com/sirupsen/logrus"
+	"errors"
 )
 
 type AuthApi struct {
@@ -31,35 +33,76 @@ func (api AuthApi) Login(ctx *fiber.Ctx) error {
 
 	var req dto.AuthRequest
 	if err := ctx.BodyParser(&req); err != nil {
+		utility.CreateLog("warn", "login failed: invalid request body", "application", logrus.Fields{
+			"route": "/v1/auth/login",
+			"reason": "invalid_body",
+			"error": err.Error(),
+			"ip": ctx.IP(),
+			"ua": ctx.Get("User-Agent"),
+		})
+
 		return ctx.Status(http.StatusBadRequest).JSON(
 			dto.CreateResponseErrorData(
 				http.StatusBadRequest,
-				"Failed created data",
+				"Failed Login.",
 				map[string]string{"body": err.Error()},
 			),
 		)
 	}
 
-	fails := utility.Validate(req)
-	if len(fails) > 0 {
-		return ctx.Status(http.StatusBadRequest).JSON(
-			dto.CreateResponseErrorData(
-				http.StatusBadRequest,
-				"Failed created data",
-				fails,
-			),
-		)
-	}
+		fails := utility.Validate(req)
+		if len(fails) > 0 {
+			utility.CreateLog("warn", "login failed: validation error", "application", logrus.Fields{
+				"route": "/v1/auth/login",
+				"reason": "validation_failed",
+				"ip": ctx.IP(),
+				"fails": fails, // Go1.21: atau kumpulkan manual
+			})
+			
+			return ctx.Status(http.StatusBadRequest).JSON(
+				dto.CreateResponseErrorData(
+					http.StatusBadRequest,
+					"Failed Login.",
+					fails,
+				),
+			)
+		}
 
 	res, err := api.authService.Login(c, req)
 	if err != nil {
-		return ctx.Status(http.StatusInternalServerError).JSON(
+		statusCode := http.StatusInternalServerError
+
+		if errors.Is(err, domain.AuthFail) {
+			statusCode = 401
+
+			utility.CreateLog("info", "login failed: invalid credentials", "activity", logrus.Fields{
+				"route": "/v1/auth/login",
+				"email": utility.MaskEmail(req.Email),
+				"ip":    ctx.IP(),
+				"ua":    ctx.Get("User-Agent"),
+				"reason":"invalid_credentials",
+			})
+    	}
+
+		utility.CreateLog("error", "login failed: internal error", "application", logrus.Fields{
+			"route": "/v1/auth/login",
+			"error": err.Error(),
+			"ip":    ctx.Context().RemoteAddr().String(),
+		})
+
+		return ctx.Status(statusCode).JSON(
 			dto.CreateResponseError(
-				http.StatusInternalServerError,
+				statusCode,
 				err.Error(),
 			),
 		)
 	}
+
+	utility.CreateLog("info", "login success", "activity", logrus.Fields{
+		"route": "/v1/auth/login",
+		"email": utility.MaskEmail(req.Email),
+		"ip":     ctx.IP(),
+	})
 
 	return ctx.Status(http.StatusOK).JSON(
 		dto.CreateResponseSuccessWithData("Successfully Login.", res),
