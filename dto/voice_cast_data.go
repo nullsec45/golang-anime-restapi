2aca1ed24e26746cb4a7f2b63054e196ef4d9cf5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-20 15:03:02 +0700

    feat(voice_cast) : add dto voice cast show data.

--- dto/voice_cast_data.go
+++ dto/voice_cast_data.go
@@ -1,12 +1,18 @@
 package dto
 
 type VoiceCastData struct {
-	Id           string  `json:"id"`
-	AnimeId      string  `json:"anime_id"`
-	CharacterId  string  `json:"character_id"`
-	PersonId     string  `json:"person_id"`
-	Language     string  `json:"language`
+	Id           string  `json:"id,omitempty"`
+	AnimeId      string  `json:"anime_id,omitempty"`
+	CharacterId  string  `json:"character_id,omitempty"`
+	PersonId     string  `json:"person_id,omitempty"`
+	Language     string  `json:"language"`
 	RoleNote     string  `json:"role_note"`
+}
+
+type VoiceCastShowData struct {
+	VoiceCastData
+	Character CharacterData `json:"character"`
+	People    PeopleData    `json:"people"`
 }
 
 type CreateVoiceCastRequest struct {
