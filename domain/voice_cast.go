7856a4193d7d4679d698f27710883ed13ad620ba
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-20 15:01:14 +0700

    feat(voice_cast) : add struct voice cast with relation domain.

--- domain/voice_cast.go
+++ domain/voice_cast.go
@@ -7,19 +7,51 @@ import (
 )
 
 type VoiceCast struct {
-	Id                     string        `db:"id"`
-	AnimeId                string        `db:"anime_id"`
-	CharacterId            string        `db:"character_id"`
-	PersonId               string        `db:"person_id"`
-	Language               string        `db:"language"`
-	RoleNote               string        `db:"role_note"`
-	CreatedAt              sql.NullTime  `db:"created_at"`
-	UpdatedAt              sql.NullTime  `db:"updated_at"`
+	Id            string `db:"id"`
+	AnimeId       string `db:"anime_id"`
+	CharacterId   string `db:"character_id"`
+	PersonId      string `db:"person_id"`
+	Language      string `db:"language"`
+	RoleNote      string `db:"role_note"`
+
+	CreatedAt     sql.NullTime  `db:"created_at"`
+	UpdatedAt     sql.NullTime  `db:"updated_at"`
 }
 
 
+type VoiceCastWithRelation struct {
+	// kolom voice_casts
+	Id          string  `db:"id"`
+	AnimeId     string  `db:"anime_id"`
+	CharacterId string  `db:"character_id"`
+	PersonId    string  `db:"person_id"`
+	Language    *string `db:"language"`
+	RoleNote    *string `db:"role_note"`
+
+	// kolom characters
+	CharacterSlug        string  `db:"character_slug"`
+	CharacterName        string  `db:"character_name"`
+	CharacterNameNative  string  `db:"character_name_native"`
+	CharacterDescription string  `db:"description"`
+
+	// tambahkan field lain kalau perlu
+	// CharacterImageURL *string `db:"character_image_url"`
+
+	// kolom people
+	PeopleSlug        string         `db:"people_slug"`
+	PeopleName        string         `db:"people_name"`
+	PeopleNameNative  string         `db:"people_name_native"`
+	PeopleBirthday    sql.NullTime   `db:"birthday"`
+	PeopleGender      dto.GenderType `db:"gender"`
+	PeopleCountry     string         `db:"country"`
+	PeopleSiteURL     string         `db:"site_url"`
+	PeopleBiography   string         `db:"biography"`
+	// PeopleImageURL *string `db:"people_image_url"`
+}
+
 type VoiceCastRepository interface {
 	FindById(ctx context.Context,  id string) (VoiceCast, error)
+	FindByAnimeId(ctx context.Context, animeId string)([]VoiceCastWithRelation, error)
 	FindUnique(ctx context.Context,  animeId string, characterId string, personId string) (VoiceCast,  bool, error)
 	Save(ctx context.Context, data *VoiceCast) error
 	Update(ctx context.Context, data *VoiceCast) error
@@ -28,6 +60,7 @@ type VoiceCastRepository interface {
 	// DeleteByPersonId(ctx context.Context, id string) error
 	DeleteById(ctx context.Context, id string) error
 }
+
 
 type VoiceCastService interface {
 	// Index(ctx context.Context, opts CharacterListOptions) (dto.Paginated[dto.CharacterData], error)
